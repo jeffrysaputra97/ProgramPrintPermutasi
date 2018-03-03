497d2589f9fc32f1a58c50d745664b5a95511720
Jeffry Saputra <jeffrysaputra97@gmail.com>
2018-03-03 17:05:22 +0700

    Done
    Menyelesaikan program

--- ProgramPrintPermutasi/src/programprintpermutasi/ProgramPrintPermutasi.java
+++ ProgramPrintPermutasi/src/programprintpermutasi/ProgramPrintPermutasi.java
@@ -12,59 +12,62 @@ import java.util.Scanner;
  *
  * @author jeffr
  */
-
 public class ProgramPrintPermutasi {
 
     /**
      * @param args the command line arguments
      */
     private static Scanner scan = new Scanner(System.in);
-    
-    private static int[] inisialisasi(int jumlah){
-        int[] result = new int[jumlah];
-        int angka;
-        
-        for (int i = 0; i < result.length; i++) {
-            System.out.print("Masukkan angka ke - " + i + ": ");
-            result[i] = scan.nextInt();
+
+    private static ArrayList<Integer> inisialisasi(int jumlah) {
+        ArrayList<Integer> result = new ArrayList<>();
+
+        for (int i = 0; i < jumlah; i++) {
+            System.out.print("Masukkan data ke - " + (i+1) + ": ");
+            result.add(scan.nextInt());
         }
-        
+
         return result;
     }
-    
-//    , int index
-    
-    private static void printPermutasi(int[] data, int indexNow, int indexSwitch){
-        int temp = data[indexSwitch];
-        
-        data[indexSwitch] = data[indexNow];
-        data[indexNow] = temp;
-        
-        if (indexSwitch == data.length - 1) {
-            for (int i = 0; i < data.length; i++) {
-                System.out.println(data[i] + " ");
-            }
-        } else {
-            printPermutasi(data, indexNow, indexSwitch + 1);
-        }
-        
+
+    private static void switchContent(ArrayList<Integer> data, int index1, int index2) {
+        int temp = data.get(index2);
+        data.set(index2, data.get(index1));
+        data.set(index1, temp);
     }
-    
+
+    private static void printPermutasi(ArrayList<Integer> data, int callerIndex) {
+        for (int i = callerIndex; i < data.size(); i++) {
+            if (callerIndex != data.size() - 1) {
+                switchContent(data, callerIndex, i);                
+                printPermutasi(data, callerIndex + 1);
+                switchContent(data, callerIndex, i);
+
+            } else {
+                for (int print : data) {
+                    System.out.print(print + " ");
+                }
+                System.out.println("");
+            }
+        }
+    }
+
     public static void main(String[] args) {
         // TODO code application logic here
         /*
             input N (jumlah objek angka yang akan dilakukan permutasi
             isi kumpulan dari objek angka sebanyak N data
             tampilkan seluruh kemungkinan permutasi
-        */
+         */
         int n;
-        
+
         System.out.print("Masukkan jumlah data: ");
         n = scan.nextInt();
-        
-        int[] listOfAngka = inisialisasi(n);
-        
-        printPermutasi(listOfAngka, 0, 0);
+
+        ArrayList<Integer> listOfNumber = inisialisasi(n);
+
+        System.out.println("\nHasil Permutasi: ");
+        printPermutasi(listOfNumber, 0);
     }
-    
+
 }
